d040fb8226b89e19bf5c7543937015d443ee243a
Olya Gwenu <oli4ka.p@gmail.com>
2019-04-13 22:14:04 +0100

    Added styles to Upload page

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    docfile = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 from .models import Greeting
+from .models import Document
+from .forms import DocumentForm
 
 # Create your views here.
 def index(request):
@@ -20,7 +26,24 @@ def db(request):
 
 
 def upload(request):
-    return render(request, "upload.html", {})
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('upload'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+
+    # Render list page with the documents and the form
+    return render(request, "upload.html", {'documents': documents, 'form': form})
 
 
 def record(request):
